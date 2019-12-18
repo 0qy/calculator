f7b43615065bfa1a4de2925057f8b1ffd42fb62e
0qy <linqianye333@gmail.com>
2019-12-18 14:47:53 +1300

    calculator draft done!!!!!

--- calculator.js
+++ calculator.js
@@ -2,30 +2,72 @@ let entriesList = [];
 let total = 0;
 let temp = '';
 
-let display = document.getElementById('display');
+let display = document.getElementById("screen");
 
-console.log(document.getElementById('display').value)
+function allClear(){
+  entriesList = [];
+  total = 0;
+  temp = '';
+  display.value = '0';
+}
+
+
+
 
 document.addEventListener ('click', function(){
-    let val = event.target.value;
-    console.log(val);
-    if(!isNaN(val) || val === '.') {
-      //  let display=document.getElementById('display');
-        display.value.val;
-    }
-})
+  let val = event.target.value;
 
-//function findButtonValue () {
-//    let val = event.target.value;
-//   console.log(val);
-//    if (!isNaN(val) || val === '.') {
-//        let display = document.getElementById('answer');
-//        display.value = val;
-//        console.log(val)
-//    }
-//}
+  function pushSymbol(){
+    entriesList.push(temp);
+    entriesList.push(val);
+    temp = '';
+  }
 
-
-
-
-
+  if(!isNaN(val) || val === '.') {
+    temp += val;
+    display.value = temp.substring(0.10);
+  } else if (val === "AC"){
+    allClear();
+  } else if (val === "CE"){
+    temp = '';
+    display.value = '0';
+  } else if (val === "+"){
+    pushSymbol();
+  } else if (val === "-"){
+    pushSymbol();
+  } else if (val === "x"){
+    pushSymbol();
+  } else if (val === "/"){
+    pushSymbol();
+  } else if (val === "equal"){
+    entriesList.push(temp);
+    temp = '';
+    nt = Number(entriesList[0]);
+    for(var i=0; i<entriesList.length; i++){
+      let nextNum = Number(entriesList[i+1]);
+      let symbol = entriesList[i];
+      if(symbol === '+'){
+        nt += nextNum;
+      } else if (symbol === '-'){
+        nt -= nextNum;
+      } else if (symbol === 'x'){
+        nt *= nextNum;
+      } else if (symbol === '/'){
+        nt /= nextNum;
+      }
+    } 
+    if(nt < 0){
+      nt = Math.abs(total) + '-';
+      display.value = total + nt;
+      entriesList = [];
+      temp = '';
+      total = 0;
+    } 
+    console.log(entriesList);
+    display.value = total + nt;  
+    total = 0; 
+    temp = JSON.stringify(nt);
+    entriesList = []; 
+  }
+}
+)
